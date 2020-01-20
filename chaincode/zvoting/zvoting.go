749259e858b03505ef9d93d7dd3f2baa17146ca4
tanmoytkd <tanmoykrishnadas@gmail.com>
2020-01-21 00:28:46 +0600

    added getRandom, createId, createElection, addCandidate, delete, getElections, getCandidates functions in smart contract

--- chaincode/zvoting/zvoting.go
+++ chaincode/zvoting/zvoting.go
@@ -6,15 +6,35 @@ import (
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	"github.com/hyperledger/fabric/protos/peer"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
 type ZVotingContract struct {
 }
 
 type User struct {
-	Id    string `json:"id"`
+	Id string `json:"id"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
+}
+
+type Election struct {
+	Id string
+	Name string
+	StartTime string
+	EndTime string
+	Doctype string
+}
+
+type Candidate struct {
+	Id string
+	Name string
+	Sign string
+	ImgAddress string
+	ElectionId string
+	Doctype string
 }
 
 func (s *ZVotingContract) Init(stub shim.ChaincodeStubInterface) peer.Response {
@@ -35,9 +55,135 @@ func (s *ZVotingContract) Invoke(stub shim.ChaincodeStubInterface) peer.Response
 		return s.Get(stub, args)
 	} else if function == "search" {
 		return s.Search(stub, args)
+	} else if function == "getRandom" {
+		return s.getRandom(stub, args)
+	} else if function == "createId" {
+		return s.createId(stub, args)
+	} else if function == "createElection" {
+		return s.createElection(stub, args)
+	} else if function == "createCandidate" {
+		return s.createCandidate(stub, args)
+	} else if function == "delete" {
+		return s.delete(stub, args);
 	}
 
 	return shim.Error("Invalid smart contract function")
+}
+
+func (s *ZVotingContract) getRandom(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	seed, _ := strconv.Atoi(args[0])
+	rand.Seed( int64(seed) )
+	return shim.Success( []byte(strconv.Itoa(rand.Int()))  )
+}
+
+// Returns an int >= min, < max
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func randomString(l int) string {
+	b := make([]rune, l)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func (s *ZVotingContract) createId(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	rand.Seed(time.Now().UnixNano())
+	var l int = 20
+	randStr := randomString(l)
+
+	value, err := stub.GetState(randStr)
+	if err != nil {
+		return shim.Error(fmt.Sprintf("Failed to get asset: %s with error: %s", args[0], err))
+	}
+	for value != nil {
+		randStr = randomString(l)
+		value, err = stub.GetState(randStr)
+	}
+
+	return shim.Success( []byte(randStr) )
+}
+
+func (s *ZVotingContract) createElection(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	fmt.Printf("INFO: create election with args: %s\n", args)
+
+	key := args[0]
+
+	election := Election{
+		Id:        key,
+		Name:      args[1],
+		StartTime: args[2],
+		EndTime:   args[3],
+		Doctype:   "Election",
+	}
+
+	electionJSON, _ := json.Marshal(election)
+	err := stub.PutState(key, electionJSON)
+	if err != nil {
+		fmt.Printf("ERROR: error PutState: %s\n", err.Error())
+		shim.Error("error PutState: " + err.Error())
+	}
+
+	return shim.Success(nil)
+}
+
+func (s *ZVotingContract) createCandidate(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	fmt.Printf("INFO: create election with args: %s\n", args)
+
+	key := args[0]
+
+	candidate := Candidate{
+		Id:         key,
+		Name:       args[1],
+		Sign:       args[2],
+		ImgAddress: args[3],
+		ElectionId: args[4],
+		Doctype:    "Candidate",
+	}
+	candidateJSON, _ := json.Marshal(candidate)
+	err := stub.PutState(key, candidateJSON)
+	if err != nil {
+		fmt.Printf("ERROR: error PutState: %s\n", err.Error())
+		shim.Error("error PutState: " + err.Error())
+	}
+
+	return shim.Success(nil)
+}
+
+func (s *ZVotingContract) delete(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	fmt.Printf("INFO: delete with key: %s\n", args)
+
+	err := stub.PutState(args[0], nil)
+	if err != nil {
+		fmt.Printf("ERROR: error PutState: %s\n", err.Error())
+		shim.Error("error PutState: " + err.Error())
+	}
+
+	return shim.Success(nil)
+}
+
+func (s *ZVotingContract) getElections(stub shim.ChaincodeStubInterface, args []string) peer.Response {
+	fmt.Printf("INFO: Get Elections")
+
+
+	iterator, err := stub.GetStateByRange(frm, to)
+	if err != nil {
+		return shim.Error("Error search by range: " + err.Error())
+	}
+	defer iterator.Close()
+
+	// build json respone
+	buffer, err := buildResponse(iterator)
+	if err != nil {
+		return shim.Error("Error constract response: " + err.Error())
+	}
+	fmt.Printf("INFO: search response:%s\n", buffer.String())
+
+	return shim.Success(buffer.Bytes())
 }
 
 func (s *ZVotingContract) Create(stub shim.ChaincodeStubInterface, args []string) peer.Response {
@@ -62,12 +208,12 @@ func (s *ZVotingContract) Create(stub shim.ChaincodeStubInterface, args []string
 func (s *ZVotingContract) Get(stub shim.ChaincodeStubInterface, args []string) peer.Response {
 	fmt.Printf("INFO: get with args: %s\n", args)
 
-	usr, _ := stub.GetState(args[0])
-	if usr == nil {
-		return shim.Error("Could not get user with id: " + args[0])
+	data, _ := stub.GetState(args[0])
+	if data == nil {
+		return shim.Error("Could not get record with id: " + args[0])
 	}
 
-	return shim.Success(usr)
+	return shim.Success(data)
 }
 
 func (s *ZVotingContract) Search(stub shim.ChaincodeStubInterface, args []string) peer.Response {
